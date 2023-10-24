0a549faeb601cc7e6ada670c4787ba0110cc8c82
Ahmed Abdallah <ahmedabdallah9012@gmail.com>
2023-10-24 22:12:24 +0300

    reducer change

--- day_1/reducer.dart
+++ day_1/reducer.dart
@@ -9,13 +9,20 @@ main() {
   int reducer(List<int> list, int Function(int, int) reducer, [int acc = 0]) {
     if (acc == 0) {
       acc = list[0];
+      for (int i = 1; i < list.length; i++) {
+        reducer(acc, list[i]);
+        acc += list[i];
+      }
+      return acc;
+    } else {
+      for (int i = 0; i < list.length; i++) {
+        reducer(acc, list[i]);
+        acc += list[i];
+      }
+      return acc;
     }
-    for (int element in list) {
-      reducer(acc, element);
-      acc += element;
-    }
-    return acc;
   }
 
   print(reducer([1, 2, 3], (value, element) => value + element));
+  //print(customReducer([1, 2, 3], (value, element) => value + element, 1));
 }
