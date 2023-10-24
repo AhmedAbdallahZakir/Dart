18250cdd695edf58c1e2bf8a75dcb5be2625e195
Ahmed Abdallah <ahmedabdallah9012@gmail.com>
2023-10-24 21:59:56 +0300

    delete file

--- day_1/reducer.dart
+++ day_1/reducer.dart
@@ -1,6 +1,21 @@
+import 'dart:math';
+
 main() {
   List lista = [1, 2, 3, 5];
-  print(lista.reduce((value, element) {
-    print(element);
-  }));
+  // print(lista.reduce((value, element) {
+  //   print(element);
+  // }));
+
+  int reducer(List<int> list, int Function(int, int) reducer, [int acc = 0]) {
+    if (acc == 0) {
+      acc = list[0];
+    }
+    for (int element in list) {
+      reducer(acc, element);
+      acc += element;
+    }
+    return acc;
+  }
+
+  print(reducer([1, 2, 3], (value, element) => value + element));
 }
